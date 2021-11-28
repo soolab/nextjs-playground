9c8eb5ea18d408717e7208070e830702cc48ed48
kimjinsoo <kimjs@dramancompany.com>
2021-11-28 13:23:33 +0900

    map with linl

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,12 +1,31 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 function ClientsProgjectPage() {
   const router = useRouter();
   console.log(router.query);
 
+  const clients = [
+    {
+      id: "jinsoo",
+      name: "kimjinsoo",
+    },
+    {
+      id: "jinsoo",
+      name: "kimjinsoo",
+    },
+  ];
+
   return (
     <div>
       <h1>The Clients Project Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
