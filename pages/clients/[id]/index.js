ac7fdc178cd39941e28f5032fdde33b7b3f9e2db
kimjinsoo <kimjs@dramancompany.com>
2021-11-28 13:32:18 +0900

    move to anther page with button and handler and push

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -16,6 +16,24 @@ function ClientsProgjectPage() {
     },
   ];
 
+  // link를 사용해도 되지만은
+  // 이렇게 button과 router.push를 사용해도 된다는 것임.
+  // Link를 활용하지 않을 경우에는 이렇게 하는 것도 나쁘지 않아보인다.
+  // 그냥 다른 방법이 있다 정도로 생각하면 될 것 같다.
+  function loadProjectHandler() {
+    // load data..
+    // router.push("/clinets/max/projecta");
+    // router.replace 를 하면은 push가 되는게 아니라 대체되는 것이라고 생각하면 될 것 같다.
+    // 이런식으로 해도 무방하다.
+    // object를 push에 전달하는 방법도 있다.
+    // string으로 바로 줄 것이냐 아니면은 mapping해서 줄 것이냐의 차이인데
+    // 나 같은 경우네는 그냥 바로 주는것이 더 깔끔해보이긴 한다.
+    router.push({
+      pathname: "clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "projecta" },
+    });
+  }
+
   return (
     <div>
       <h1>The Clients Project Page</h1>
@@ -24,6 +42,7 @@ function ClientsProgjectPage() {
           <li key={client.id}>
             {/* 이렇게 href를 string으로 넣어도 되긴 하지만, object로 넘길 수 있음. */}
             {/* <Link href={`/clients/${client.id}`}>{client.name}</Link> */}
+            {/* 아래처럼 해도 된다. 어떻게 할지는 알아서 정하도록 하자. */}
             <Link
               href={{ pathname: "/clients/[id]", query: { id: client.id } }}
             >
@@ -32,6 +51,7 @@ function ClientsProgjectPage() {
           </li>
         ))}
       </ul>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
